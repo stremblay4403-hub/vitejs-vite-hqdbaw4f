79693d5e0ca5037ce5b303562b53a222001016fb
stremblay4403-hub <stremblay4403@bell.net>
2026-06-26 19:14:29 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,3 +4,13 @@ import App from './App'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <App />
 )
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js');
+  });
+}
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js');
+  });
+}
