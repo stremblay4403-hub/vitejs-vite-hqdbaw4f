88b9ab5a6c3e169e18073e5aebb94a6bfd2aba5c
stremblay4403-hub <stremblay4403@bell.net>
2026-06-26 19:27:42 -0400

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,20 @@
-self.addEventListener('fetch', () => {});
+const CACHE_NAME = 'tdv-images-v1';
+
+self.addEventListener('fetch', (event) => {
+  const req = event.request;
+  if (req.destination === 'image') {
+    event.respondWith(
+      caches.open(CACHE_NAME).then(async (cache) => {
+        const cached = await cache.match(req);
+        const networkFetch = fetch(req)
+          .then((res) => {
+            cache.put(req, res.clone());
+            return res;
+          })
+          .catch(() => cached);
+        return cached || networkFetch;
+      })
+    );
+  }
+});
+
